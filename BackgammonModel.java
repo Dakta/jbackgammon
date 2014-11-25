ef025b8366a49c924278303ecfb3c2fadf9b7ccc
thatguywhorows <siadak.griffin@gmail.com>
2014-11-24 22:14:47 -0800

    First part of constructor
    I think i’m getting somewhere

--- BackgammonModel.java
+++ BackgammonModel.java
@@ -2,19 +2,20 @@ package jbackgammon;
 
 import java.awt.Color;
 
-public class BackgammonModel<playerOne ,playerTwo> {
+public class BackgammonModel<playerOne, playerTwo> {
 
 	
+	static final int empty = 0;
+	static final int white = 1;
+	static final int black = 2;
 	// current player
 	private Color currentPlayer;
-	// board position
-	// Array of Points<Color>, is an int
-	
-	//Which point
+
+	// Which point
 	int count[];
-	//Type of point
-	int type[];
-	
+	// Type of point
+	int color[];
+
 	// white rail int
 	// black rail int
 	int white_rail = 0;
@@ -23,26 +24,41 @@ public class BackgammonModel<playerOne ,playerTwo> {
 	// black home int
 	int white_home = 0;
 	int black_home = 0;
+
+	// board position
+	// Array of Points<Color>, is an int
+	// Builds initial board (constructor, if you will)
+
+	public BackgammonModel(){
+		count = new int[24];
+		color = new int[24];
+		
+		for (int i = 0; i < 24; i++){
+			count[i] = 0;
+			color[i] = empty;
+		}
+	}
+
 	// move piece from board to board
 	// decrement from
 	// increment to
-	
+
 	// bear piece off
 	// can bear off?
 	// decrement from
 	// increment home
-	
+
 	// blotting
 	// decrement from
 	// increment rail
-	
+
 	// enter from rail
 	// decrement rail
 	// increment to
-	
+
 	// has won?
 	// white rail == 15
 	// black rail == 15
 	// null
-	
+
 }
