369e0e83c5ac07e7dae6541a5c734ce70bce9a76
Dakota Schneider <dakota@hackthetruth.org>
2014-11-24 20:40:13 -0800

    model rough outline

---
+++ BackgammonModel.java
@@ -0,0 +1,31 @@
+package jbackgammon;
+
+public class BackgammonModel {
+
+	// current player
+	
+	// array of white's pieces
+	// array of black's pieces
+	
+	// board position
+	// Array of Points<Color>, which contain List<Color> of Piece<Color>
+	
+	// white rail
+	// black rail
+	
+	// white home
+	// black home
+	
+	// move piece from board to board
+	
+	// bear piece off
+	// can bear off?
+	
+	// blotting
+	
+	// enter from rail
+	
+	// has won?
+	
+	
+}
