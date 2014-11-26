c2c6ef30637868e43d30892222bc41782d9dd331
thatguywhorows <siadak.griffin@gmail.com>
2014-11-25 18:44:34 -0800

    Consolidated
    Added all of the methods and constants from the dice class to the
    model. No need to have it in a separate class. Also added a shape to
    the board that will hopefully become the dice roll button. Still
    working on how that is going to happen.

--- Backgammon.java
+++ Backgammon.java
@@ -1,6 +1,11 @@
 package jbackgammon;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
 
 import klondikeSolitaire.Deck;
 import edu.princeton.cs.introcs.StdDraw;
@@ -9,47 +14,52 @@ public class Backgammon {
 
 	public static void main(String[] args) {
 		// choose rules
-		
+
 		// roll for first
-		
+
 		// initialize model and play
-		 new Backgammon().run();
+		new Backgammon().run();
 	}
-	
+
 	private BackgammonModel model;
 	private int currentPlayer;
 	private boolean waitingForSource;
 	private int currentPoint;
-	
+
 	public Backgammon() {
 		this(BackgammonModel.black);
 	}
-	
+
 	public Backgammon(int startingPlayer) {
 		this.model = new BackgammonModel();
 		this.currentPlayer = startingPlayer;
 		this.waitingForSource = true;
 		this.currentPoint = 0;
+
 	}
-	
+
 	public void draw() {
 		// draw board
 		drawBoard();
 		// draw pieces
 		int x = 0;
 		int y = 0;
-		for (int i=0; i<model.count.length; i++) {
+		for (int i = 0; i < model.count.length; i++) {
 			// draw spike
-			double[] xi = {baseUnit*i, 0.5*(baseUnit*i+baseUnit*(i+1)), baseUnit*(i+1)};
-			double[] yi = {0, 5*baseUnit, 0};
-			StdDraw.setPenColor((i%2==0 ? DARK_BROWN : LIGHT_BROWN));
+			double[] xi = { baseUnit * i,
+					0.5 * (baseUnit * i + baseUnit * (i + 1)),
+					baseUnit * (i + 1) };
+			double[] yi = { 0, 5 * baseUnit, 0 };
+			StdDraw.setPenColor((i % 2 == 0 ? DARK_BROWN : LIGHT_BROWN));
 			StdDraw.filledPolygon(xi, yi);
-			
+
 			// draw stack of pieces
 			y = 0;
-			for (int c=0; c<model.count[i]; c++) {
-				StdDraw.setPenColor((model.color[i] == model.white ? StdDraw.WHITE : StdDraw.BLACK));
-				StdDraw.filledCircle(x+0.5*baseUnit, y+0.5*baseUnit, 0.5*baseUnit);
+			for (int c = 0; c < model.count[i]; c++) {
+				StdDraw.setPenColor((model.color[i] == model.white ? StdDraw.WHITE
+						: StdDraw.BLACK));
+				StdDraw.filledCircle(x + 0.5 * baseUnit, y + 0.5 * baseUnit,
+						0.5 * baseUnit);
 				// move up one
 				y += baseUnit;
 			}
@@ -58,34 +68,38 @@ public class Backgammon {
 		}
 
 		// draw rails
-		x = 2*baseUnit;
-		y = 11*baseUnit;
-		StdDraw.text(baseUnit, 10.5*baseUnit, "Rail");
-		StdDraw.setPenColor(StdDraw.WHITE );
-		for (int i=0; i<model.white_rail; i++) {
-			StdDraw.filledCircle(x+0.5*baseUnit, y, 0.5*baseUnit);
+		x = 2 * baseUnit;
+		y = 11 * baseUnit;
+		StdDraw.text(baseUnit, 10.5 * baseUnit, "Rail");
+		StdDraw.setPenColor(StdDraw.WHITE);
+		for (int i = 0; i < model.white_rail; i++) {
+			StdDraw.filledCircle(x + 0.5 * baseUnit, y, 0.5 * baseUnit);
 			x += baseUnit;
 		}
 		// reset
-		x = 2*baseUnit;
-		y = 10*baseUnit;
-		StdDraw.setPenColor(StdDraw.BLACK );
-		for (int i=0; i<model.black_rail; i++) {
-			StdDraw.filledCircle(x+0.5*baseUnit, y, 0.5*baseUnit);
+		x = 2 * baseUnit;
+		y = 10 * baseUnit;
+		StdDraw.setPenColor(StdDraw.BLACK);
+		for (int i = 0; i < model.black_rail; i++) {
+			StdDraw.filledCircle(x + 0.5 * baseUnit, y, 0.5 * baseUnit);
 			x += baseUnit;
 		}
 
 		StdDraw.setPenColor(StdDraw.WHITE);
-		StdDraw.text(10*baseUnit, 10.5*baseUnit, "Current Player");
-		StdDraw.setPenColor(currentPlayer == model.black ? StdDraw.BLACK : StdDraw.WHITE);
-		StdDraw.filledCircle(12.5*baseUnit, 10.5*baseUnit, 0.5*baseUnit);
-		
+		StdDraw.text(10 * baseUnit, 10.5 * baseUnit, "Current Player");
+		StdDraw.setPenColor(currentPlayer == model.black ? StdDraw.BLACK: StdDraw.WHITE);
+		StdDraw.filledCircle(12.5 * baseUnit, 10.5 * baseUnit, 0.5 * baseUnit);
+
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.filledRectangle(20 * baseUnit, 10.5 * baseUnit, baseUnit,0.5 * baseUnit);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.text(20 * baseUnit, 10.5 * baseUnit, "Roll Dice");
 		// draw moving piece under mouse
 		drawCurrentPiece();
-		
+
 		StdDraw.show(40);
 	}
-	
+
 	private static final Color BROWN = new Color(97, 68, 14);
 	private static final Color LIGHT_BROWN = new Color(181, 139, 62);
 	private static final Color DARK_BROWN = new Color(115, 87, 36);
@@ -98,25 +112,29 @@ public class Backgammon {
 
 	private void drawCurrentPiece() {
 		if (!waitingForSource) {
-			StdDraw.setPenColor(currentPlayer == model.black ? StdDraw.BLACK : StdDraw.WHITE);
-			StdDraw.filledCircle(StdDraw.mouseX(), StdDraw.mouseY(), 0.6*baseUnit);
+			StdDraw.setPenColor(currentPlayer == model.black ? StdDraw.BLACK
+					: StdDraw.WHITE);
+			StdDraw.filledCircle(StdDraw.mouseX(), StdDraw.mouseY(),
+					0.6 * baseUnit);
 		}
 	}
-	
+
 	private void drawBoard() {
 		// Background
 		StdDraw.clear(BLACK);
 		StdDraw.setPenColor(BACKGROUND);
-		StdDraw.filledRectangle(model.count.length*baseUnit/2, 12*baseUnit/2, model.count.length*baseUnit/2, 12*baseUnit/2);
-		
+		StdDraw.filledRectangle(model.count.length * baseUnit / 2,
+				12 * baseUnit / 2, model.count.length * baseUnit / 2,
+				12 * baseUnit / 2);
+
 	}
 
 	public static void drawSpike(int start, Color color1, Color color2,
 			double[] x, double[] y) {
 		int count = 0;
-//		if (start == 1){
-//			count = -1;
-//		}
+		// if (start == 1){
+		// count = -1;
+		// }
 		while (count < 3) {
 			if (start == 0) {
 				StdDraw.setPenColor(color1);
@@ -137,21 +155,22 @@ public class Backgammon {
 			count++;
 		}
 	}
-	
+
 	/** Returns the point under the mouse. */
 	public int mousePoint() {
-//		int result = (int) Math.round(StdDraw.mouseX() / model.count.length / 2 + 1);
+		// int result = (int) Math.round(StdDraw.mouseX() / model.count.length /
+		// 2 + 1);
 		int result = (int) StdDraw.mouseX() / baseUnit + 1;
 		return result;
 	}
-	
+
 	/** Plays the game. */
 	public void run() {
 		// only do this once
-		StdDraw.setCanvasSize(model.count.length*baseUnit, 12*baseUnit);
-		StdDraw.setXscale(0, model.count.length*baseUnit);
-		StdDraw.setYscale(0, 12*baseUnit);
-		
+		StdDraw.setCanvasSize(model.count.length * baseUnit, 12 * baseUnit);
+		StdDraw.setXscale(0, model.count.length * baseUnit);
+		StdDraw.setYscale(0, 12 * baseUnit);
+
 		while (true) {
 			while (!StdDraw.mousePressed()) {
 				// Wait for mouse press
@@ -162,8 +181,9 @@ public class Backgammon {
 				waitingForSource = false;
 			} else { // Waiting for destination
 				waitingForSource = true;
-				model.move(currentPoint-1, mousePoint()-1);
-				currentPlayer = currentPlayer == BackgammonModel.black ? BackgammonModel.white : BackgammonModel.black;
+				model.move(currentPoint - 1, mousePoint() - 1);
+				currentPlayer = currentPlayer == BackgammonModel.black ? BackgammonModel.white
+						: BackgammonModel.black;
 			}
 			while (StdDraw.mousePressed()) {
 				// Wait for mouse release
--- BackgammonModel.java
+++ BackgammonModel.java
@@ -1,5 +1,7 @@
 package jbackgammon;
 
+import edu.princeton.cs.introcs.StdRandom;
+
 
 /*==============================================================================
  * The viewing angle for this whole thing is from the view on the white home 
@@ -206,6 +208,46 @@ public class BackgammonModel {
 			return true;
 		}
 		return false;
+	}
+	
+	int dice1;
+	int dice2;
+
+	boolean rolled = false;
+	
+	//actually rolls the dice
+	public void rollDice() {
+		dice1 = StdRandom.uniform(1, 7);
+		dice2 = StdRandom.uniform(1, 7);
+		rolled = true;
+	}
+	//getters which i know we will need
+	public int getDice1(){
+		return dice1;
+	}
+	
+	public int getDice2(){
+		return dice2;
+	}
+	//reset the dice for next roll
+	public void resetDice(){
+		dice1 = 0;
+		dice2 = 0;
+		rolled = false;
+	}
+	//checks for doubles
+	public boolean isDoubles(int dice1, int dice2){
+		if (dice1 == dice2){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	//sets the dice to values for drawing
+	public void setDiceValues(int roll1, int roll2){
+		dice1 = roll1;
+		dice2 = roll2;
 	}
 
 }
